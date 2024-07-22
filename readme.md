f04eaf46ce3883864814f37f03cae185f0a7fe5f
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-07-22 17:03:57 -0300

    Backend inicia com o front

--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
-Será preciso iniciar o node com `npm i` 
-Depois rode a nova biblioteca de conexão com o db `npm install mysql2`
-Faça as alterações necessárias na pasta "database.js"
-Rode tudo com `node server`
+Será preciso iniciar o node com `npm i` <br>
+Depois rode a nova biblioteca de conexão com o db `npm install mysql2`<br>
+Faça as alterações necessárias na pasta "database.js"<br>
+Rode tudo com `node server`<br>
+
+
+O Fontend vai rodar na porta 3001 <br>
+O backend vai rodar na porta 3000
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-var app = require('./config/express');
+var backend = require('./config/express');
+const express = require('express');
+const path = require('path');
+const app = express();
+const porta = 3001;
 
 var port = process.env.PORT || 3000;
+
+// Servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota principal que serve o arquivo index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(porta, () => {
+    console.log(`Servidor rodando em http://localhost:${porta}`);
+  });
     
-app.listen(port, function(){
+backend.listen(port, function(){
     console.log('Server ON in Port ' + port);
 })
