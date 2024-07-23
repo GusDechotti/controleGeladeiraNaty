55420ec39f18246bd2815860acd09b39696360e1
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-07-23 15:34:53 -0300

    Fluxo principal concluido

---
+++ public/btnProduto.js
@@ -0,0 +1,59 @@
+const apiUrl = 'http://localhost:3000/api/produto';
+
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+async function addBtnProduto() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        const container = document.getElementById('btnProduto');
+
+        data.forEach(item => {
+            const btn = document.createElement('button');
+            var img = document.createElement('img');
+            img.src = item.imagem;
+            img.alt = item.nome;
+            img.style.width = '100px';
+            img.style.height = '100px';
+            var text = document.createTextNode(item.nome);
+            btn.appendChild(img);
+            btn.appendChild(text);
+            btn.createElement
+            btn.addEventListener('click', () => {
+                const url = 'http://localhost:3000/api/pedido';
+                const data = {
+                    "id_pessoa": getQueryParam("id"),
+                    "id_produto": item.id
+                };
+                fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Erro na requisição');
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        const newUrl = `confirmaCompra.html`;
+                        window.location.href = newUrl;
+                    })
+                    .catch(error => {
+                        console.error('Erro:', error);
+                    });
+            });
+            container.appendChild(btn);
+        });
+    } catch (error) {
+        console.error('Ocorreu um erro:', error);
+    }
+}
+addBtnProduto();
---
+++ public/script.js
@@ -0,0 +1,24 @@
+const apiUrl = 'http://localhost:3000/api/pessoa';
+
+async function adicionaBtnPessoa() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    const container = document.getElementById('button-container');
+
+    data.forEach(item => {
+      const button = document.createElement('button');
+      button.setAttribute('nome', item.nome);
+      button.textContent = item.nome;
+      button.addEventListener('click', () => {
+        const newUrl = `produto.html?id=${item.id}`;
+        window.location.href = newUrl;
+      });
+      container.appendChild(button);
+    });
+  } catch (error) {
+    console.error('Ocorreu um erro:', error);
+  }
+}
+adicionaBtnPessoa();
