4a861ce32bc7012ee099035dc23fe7366b967906
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-08-03 14:41:07 -0300

    Listagem compras para a Jake

--- public/js/jake.js
+++ public/js/jake.js
@@ -1,7 +1,10 @@
-function cadastrarProduto(){
-    const newUrl = `/html/cadastrarProduto.html`;
-    window.location.href = newUrl;
+function cadastrarProduto() {
+  const newUrl = `/html/cadastrarProduto.html`;
+  window.location.href = newUrl;
 }
+
+var Pix = document.getElementById("isPix");
+var isPix = Pix.checked; 
 
 const apiUrl = 'http://localhost:3000/api/pedido';
 
@@ -12,9 +15,17 @@ async function exibePedidos() {
     const container = document.getElementById('tabela-container');
     data.forEach(item => {
       console.log(item)
+      
       const tr = document.createElement('tr');
-      tr.setAttribute('id',item.pedidoId)
+      tr.setAttribute('id', item.pedidoId)
       container.appendChild(tr);
+      const tdNome = document.createElement('td');
+      tdNome.textContent = item.nomePessoa
+      const onde = document.getElementById(item.pedidoId)
+      onde.appendChild(tdNome)
+      const tdProduto = document.createElement('td');
+      tdProduto.textContent = item.nomeProduto
+      onde.appendChild(tdProduto)
     });
   } catch (error) {
     console.error('Ocorreu um erro:', error);
