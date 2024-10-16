b2ca37c28ec149cb18330f087d47bd500fd31ce0
Conrado <arthurconradoarruda@hormail.com>
2024-10-16 15:51:44 -0300

    Adicionado Timer nas págida Tela Inicial e Confirma Compra, Adicionado legendas aos ícones página Admin

---
+++ public/js/temporizador.js
@@ -0,0 +1,4 @@
+setTimeout(function() {
+    // Redireciona para a página desejada após o tempo esgotar
+    window.location.href = "../html/home.html"; // Substitua com o caminho da página desejada
+}, 60000);
