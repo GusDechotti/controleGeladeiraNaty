023d023ee112bdf7f55765b8598d098f3057b5cd
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2025-02-21 18:19:14 -0300

    Correcao disparo naty

--- app/controllers/disparo.controller.js
+++ app/controllers/disparo.controller.js
@@ -15,7 +15,7 @@ exports.compra = async (req, res) => {
     const url = 'https://api.staging.naty.app/api/v2/campaigns/instantly';
     const data = {
         "name": "Compra na geladeira",
-        "whatsappId": "44c248fa-a49f-4c5e-9871-99f46e3341f9",
+        "whatsappId": "875dfec6-7ee3-4031-90e9-3041f1b6431c",
         "queueId": "54c8bfd1-1c2d-4d41-95e8-6f5c2da484b5",
         "ticketStatus": "closed",
         "messages": [
@@ -57,7 +57,7 @@ exports.compraPix = async (req, res) => {
     const url = 'https://api.staging.naty.app/api/v2/campaigns/instantly';
     const data = {
         "name": "Compra na geladeira",
-        "whatsappId": "44c248fa-a49f-4c5e-9871-99f46e3341f9",
+        "whatsappId": "875dfec6-7ee3-4031-90e9-3041f1b6431c",
         "queueId": "54c8bfd1-1c2d-4d41-95e8-6f5c2da484b5",
         "ticketStatus": "closed",
         "minMsgInterval":500,
