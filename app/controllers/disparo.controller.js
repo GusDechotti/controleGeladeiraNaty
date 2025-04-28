8c2441a52ab29498b76c4823b70e1203fd92e3bd
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2025-04-28 17:19:07 -0300

    Correção WhatsId

--- app/controllers/disparo.controller.js
+++ app/controllers/disparo.controller.js
@@ -15,7 +15,7 @@ exports.compra = async (req, res) => {
     const url = 'https://api.staging.naty.app/api/v2/campaigns';
     const data = {
         "name": "Compra na geladeira",
-        "whatsappId": "ba1799e0-cd52-4685-8dab-124b7b0a2b97",
+        "channelId": "b22b712d-3fec-4caf-9564-407ce1575031",
         "queueId": "54c8bfd1-1c2d-4d41-95e8-6f5c2da484b5",
         "ticketStatus": "closed",
         "minMsgInterval": 100,
@@ -61,7 +61,7 @@ exports.compraPix = async (req, res) => {
     const url = 'https://api.staging.naty.app/api/v2/campaigns';
     const data = {
         "name": "Compra na geladeira",
-        "whatsappId": "ba1799e0-cd52-4685-8dab-124b7b0a2b97",
+        "channelId": "b22b712d-3fec-4caf-9564-407ce1575031",
         "queueId": "54c8bfd1-1c2d-4d41-95e8-6f5c2da484b5",
         "ticketStatus": "closed",
         "minMsgInterval": 100,
